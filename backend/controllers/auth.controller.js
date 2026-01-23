1fdb412439c0e7bc23e0a75d7a5370b3fc761423
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2026-01-23 19:24:08 +1100

    add user models and validation check for signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,20 @@
-export const signup = (req, res) => {
+import User from '../models/user.models.js';
+
+export const signup = async (req, res) => {
     try {
         const {fullName, password, confirmPassword, gender} = req.body;
+        
+        if (password != confirmPassword) {
+            return res.status(400).json({error:"Passwords don't match"})
+        }
+        const user = await User.findOne({username});
+
+        if (user) {
+            return res.status(400).json({error: "username already exist"})
+        }
+        
     } catch (error) {}
+
     res.send("signupUser");
     console.log("signupUser");
 };
