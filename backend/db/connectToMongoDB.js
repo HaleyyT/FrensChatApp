7972ba2b2847c73d45c32036fdf7d280f89070d3
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2026-01-25 17:11:23 +1100

    fix syntax error to connect to mongoDB successfully

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,9 +1,11 @@
-import mongoese from 'mongoose';
+import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
     try {
-        await mongoese.connect(process.env.MONGO_URI); 
+        await mongoose.connect(process.env.MONGO_URI); 
         console.log("Connected to mongoDB");
+        console.log("Mongo host:", mongoose.connection.host);
+        console.log("Mongo db:", mongoose.connection.name);
     } catch(error) {
         console.error('Error connecting to MongoDB:', error.message);
     }
