7145e34863eb3e9591fe428b466c498104de67a6
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2026-01-25 17:12:45 +1100

    (backend/auth-signup): add error handling for creating user and fix syntax error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,27 +2,31 @@ import User from '../models/user.models.js';
 
 export const signup = async (req, res) => {
     try {
-        const {fullName, password, confirmPassword, gender} = req.body;
+        const {fullName, username, password, confirmPassword, gender} = req.body;
         
-        if (password != confirmPassword) {
-            return res.status(400).json({error:"Passwords don't match"})
+        if (!fullName || !username || !password || !confirmPassword || !gender) {
+            return res.status(400).json({error: "Missing required fields"});
+        }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({error:"Passwords don't match"});
         }
         const user = await User.findOne({username});
 
         if (user) {
-            return res.status(400).json({error: "username already exist"})
+            return res.status(400).json({error: "username already exist"});
         }
         // Hash password
         // https://avatar-placeholder.iran.liara.run/
-        const boyProfilePic = 'https://avatar.iran.liara.run/public/boy?username=$(username)'
-        const girlProfilePic = 'https://avatar.iran.liara.run/public/girl?username=$(username)'
+        // const boyProfilePic = 'https://avatar.iran.liara.run/public/boy?username=$(username)'
+        // const girlProfilePic = 'https://avatar.iran.liara.run/public/girl?username=$(username)'
         
         const newUser = new User ({
             fullName,
             username,
             password,
             gender,
-            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+            // profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
     await newUser.save();
 
@@ -30,12 +34,12 @@ export const signup = async (req, res) => {
         _id: newUser._id,
         fullName: newUser.fullName,
         username: newUser.username,
-        profilePic: newUser.profilePic,
+        //profilePic: newUser.profilePic,
     })
     
     } catch (error) {
-        console.log("Error in signup controller", error.message)
-        res.status(500).json({error: "Internal server error"})
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error: "Internal server error"});
     }
 };
 
