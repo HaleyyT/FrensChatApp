76f95d0b1276b69b46f7e2fcdd4754a4ea2cfafd
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2026-02-10 15:38:57 +0700

    fix(auth): adjust jwt cookie options for local dev

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,10 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //prevent  XSS attacks cross-site scripting attacks
-        sameSite: "strict", //CSRF attacks cross-site request forgery attacks
+        //sameSite: "strict", //CSRF attacks cross-site request forgery attacks
         //secure: process.env.NODE_ENV !== "development",
+        //secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
 
     });
